37848c67443ef370323148183b0a6047f436a5d6
ReactiveRaven <reactiveraven@gmail.com>
2014-09-08 00:50:36 +0100

    fixes for tooltip logic

--- www/js/controllers/ChampionCtrl.js
+++ www/js/controllers/ChampionCtrl.js
@@ -29,12 +29,65 @@ angular.module('starter.controllers.championctrl', ['starter.services'])
         }
 
         function replaceAll(string, find, replace) {
-            return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+            return string.replace(new RegExp(escapeRegExp(find) + "%?", 'g'), replace);
+        }
+
+        var linkTypes = {
+            "spelldamage": " Ability Power",
+            "mana": " Max Mana",
+            "bonusattackdamage": " Bonus Attack Damage",
+            "attackdamage": " Attack Damage",
+            "@stacks": " Stacks",
+            "@cooldownchampion": " Seconds",
+            "@dynamic.attackdamage": " Attack Damage",
+            "@special.dariusr3": "",
+            "@text": "",
+            "@dynamic.abilitypower": " Ability Power",
+            "@special.jaxrarmor": "",
+            "@special.jaxrmr": "",
+            "bonusarmor": " Bonus Armor",
+            "@special.nautilusq": "",
+            "bonushealth": " Bonus Health"
+        };
+
+        var noFormatNumber = [
+            "@cooldownchampion",
+            "@special.dariusr3",
+            "@text",
+            "@stacks"
+        ];
+
+        var replacements = {
+            "@Effect3Amount*-100@": "LISSANDRA16/19/22/25/28%<!--Lissandra-->",
+            "@Effect2Amount*-100@": "LISSANDRA20/30/40%"
+        };
+
+        var reduceIfSameValues = function reduceIfSameValues(prev, curr) {
+            return (prev && prev === curr ? prev : null);
+        }
+
+        function formatVariable(variable, spell) {
+            var value = (variable.coeff + "").split(",").map(function(value) { return Math.floor(value * 100); }).join("/"),
+                label = typeof linkTypes[variable.link] !== "undefined" ? linkTypes[variable.link] + "<!--" + variable.link + "-->" : variable.link;
+
+            // Simplify from 'x/x/x/x' to 'x'
+            if (value.split("/").reduce(reduceIfSameValues)) {
+                value = value.split("/")[0];
+            }
+            if ((variable.coeff + "").split(",").reduce(reduceIfSameValues)) {
+                variable.coeff = (variable.coeff + "").split(",")[0];
+            }
+
+            return (noFormatNumber.indexOf(variable.link) > -1 ? variable.coeff : value + "%") + label;
         }
 
         function processSpellDescription(spell) {
             var description = spell.tooltip,
                 regex = /\{\{ [0-9a-z]+ \}\}/g;
+
+            Object.keys(replacements).forEach(function(key) {
+                description = replaceAll(description, key, replacements[key]);
+            })
 
             spell.effectBurn.forEach(function(val, key) {
                 var search = "{{ e" + key + " }}",
@@ -44,9 +97,10 @@ angular.module('starter.controllers.championctrl', ['starter.services'])
             });
 
             spell.vars.forEach(function(variable) {
-                var search = "{{ " + variable.key + "}}",
-                    replace = variable.coeff;
-                if (replace + "" === "0") {
+                var search = "{{ " + variable.key + " }}",
+                    rounded = Math.floor(variable.coeff * 100);
+                    replace = formatVariable(variable); //(rounded ? rounded + "%" : variable.coeff) + " " + (linkTypes[variable.link] ? linkTypes[variable.link] : variable.link);
+                if (variable.coeff + "" === "0") {
                     replace = "";
                 }
                 description = replaceAll(description, search, replace);
@@ -56,7 +110,7 @@ angular.module('starter.controllers.championctrl', ['starter.services'])
                 description.match(regex).forEach(function(missing) {
                     console.error("Could not match variable '" + missing + "' in spell '" + spell.id + "'");
 
-                    description = replaceAll(description, missing, "~?~");
+                    description = replaceAll(description, missing, "<!--" + missing + "-->");
                 });
             }
 
